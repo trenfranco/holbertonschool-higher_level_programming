d0f879edfaccfa29f88fdc8fd2718cea5d39d9f7
trenfranco <trenfranco@gmail.com>
2022-04-26 16:54:45 -0700

    ex 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,9 +2,10 @@
 def print_last_digit(number):
     if number >= 0:
         number = number % 10
+        print(f"{number:d}", end="")
         return number
     else:
         a = abs(number)
         a = a % 10
-        a = - a
+        print(f"{a:d}", end="")
         return a
