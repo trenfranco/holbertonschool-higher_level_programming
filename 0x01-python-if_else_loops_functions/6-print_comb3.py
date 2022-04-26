94cc02ba7da6b4ee340cc0050a4c560a00ff5c1b
trenfranco <trenfranco@gmail.com>
2022-04-26 12:46:20 -0700

    ex 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,9 @@ n1 = 0
 n2 = 0
 
 for n1 in range(0, 10):
-    for n2 in range(0, 10):
-        if n1 != n2 and n1 < n2 and (n1 != 8 and n2 != 9):
-            print(f"{n1:d}", end="")
-            print(f"{n2:d}", end=", ")
+    for n2 in range(n1, 10):
+        if n1 != n2 and n1 < n2:
+            if (n1 != 8):
+                print(f"{n1:d}", end="")
+                print(f"{n2:d}", end=", ")
 print("89")
