e2304571bd20c9a34a14f0141662942d33a9ba4d
trenfranco <trenfranco@gmail.com>
2022-04-26 16:30:04 -0700

    ex 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    num = 0
+    for element in str:
+        num = ord(element)
+        if num > 96 and num < 123:
+            num = num - 32
+            print(f"{num:c}", end="")
+    print()
