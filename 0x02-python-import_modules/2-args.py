6c517c77b37697850038c736875f6c57b1f77167
trenfranco <trenfranco@gmail.com>
2022-04-28 11:02:24 -0700

    ex 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    argsnum = len(sys.argv)
+
+    if argsnum == 2:
+        print(f"{argsnum - 1:d} argument.")
+        print(f"1: {str(sys.argv[1])}")
+
+    elif argsnum > 2:
+        print(f"{argsnum - 1:d} arguments.")
+        i = 1
+        while i < argsnum:
+            print(f"{i:d}: {str(sys.argv[i])}")
+            i += 1
+    else:
+        print("0 arguments.")
