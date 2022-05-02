eef78756092529acbea6ba6afc862b2a23705f85
trenfranco <trenfranco@gmail.com>
2022-05-02 13:22:24 -0700

    ex 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print(j, end="")
+        for cont in i:
+            if i[-1] == cont:
+                print(f"{cont:d}", end="")
+            else:
+                print(f"{cont:d}", end=" ")
         print()
