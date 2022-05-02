884bd2582d93a62fda78912b4ca04e46a7f577e0
trenfranco <trenfranco@gmail.com>
2022-05-02 11:10:56 -0700

    ex 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,6 +5,6 @@ def new_in_list(my_list, idx, element):
     size = len(my_list) - 1
     if idx < 0 or idx > size:
         return my_list
-    a = my_list
+    a = my_list[:]
     a[idx] = element
     return a
