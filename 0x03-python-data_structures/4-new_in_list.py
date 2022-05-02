6adc7b9f4d8f09aa01e8f2522cf86e5d4e971358
trenfranco <trenfranco@gmail.com>
2022-05-02 10:59:23 -0700

    ex 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return
+    size = len(my_list) - 1
+    if idx < 0 or idx > size:
+        return my_list
+    a = my_list
+    a[idx] = element
+    return a
