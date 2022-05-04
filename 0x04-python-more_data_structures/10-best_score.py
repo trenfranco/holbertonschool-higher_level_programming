841ad8683ef90d597ade1949aab751c1f8dfd275
trenfranco <trenfranco@gmail.com>
2022-05-04 15:28:10 -0700

    ex 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max = -500
-    if a_dictionary is None:
-        return None
-    for i in list(a_dictionary):
-        if (a_dictionary[i] > max):
-            max = a_dictionary[i]
-            maxname = i
-    return maxname
+    max = -255
+    nombre = None
+    if a_dictionary:
+        for i in list(a_dictionary):
+            if (a_dictionary[i] > max):
+                max = a_dictionary[i]
+                nombre = i
+    return nombre
