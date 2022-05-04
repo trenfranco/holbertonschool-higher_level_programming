4b224690f779f361f82134990deaef673c9e261e
trenfranco <trenfranco@gmail.com>
2022-05-04 12:48:53 -0700

    ex 03

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    if set_1 == None or set_2 == None:
+        return None
     hola=()
     hola = set_1.intersection(set_2)
     return hola
