07a415cea3526e2ae6ce4ce1d57624f78ea47aa2
trenfranco <trenfranco@gmail.com>
2022-05-04 17:52:28 -0700

    ex 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or not roman_string:
+    if type(roman_string) is not str or roman_string is None:
         return 0
     res = 0
 
