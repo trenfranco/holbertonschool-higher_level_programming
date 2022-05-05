d3cdd1530e411dd6648636977362e5679af7bd8e
trenfranco <trenfranco@gmail.com>
2022-05-04 17:49:47 -0700

    ex 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,5 +6,8 @@ def roman_to_int(roman_string):
 
     roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
     for i in range(0, len(roman_string)):
-        res += roman[roman_string[i]]
+        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
+            res += roman[roman_string[i]] - 2
+        else:
+            res += roman[roman_string[i]]
     return res
