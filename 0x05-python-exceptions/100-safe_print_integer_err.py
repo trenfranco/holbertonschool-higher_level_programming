31046905969004cf138589770cd3396e362dcbe2
trenfranco <trenfranco@gmail.com>
2022-05-16 16:15:51 -0700

    ex 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         print()
         return True
     except (TypeError, ValueError) as e:
-        print(e)
+        print(e, file=sys.stderr)
         return False
