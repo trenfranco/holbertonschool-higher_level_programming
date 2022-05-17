9bbba2921fb4305cd909c0a9339780247b98792b
trenfranco <trenfranco@gmail.com>
2022-05-17 13:50:06 -0700

    ex 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,29 @@
+class Square:
+    """Private instance attribute: size"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
