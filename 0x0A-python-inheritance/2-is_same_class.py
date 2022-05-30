d7afca9b312ea6ab27d93a6258de01741e3766bf
trenfranco <trenfranco@gmail.com>
2022-05-30 14:44:43 -0700

    ex 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that checks id  object is exactly
+    an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """returns true or false"""
+    return (isinstance(obj, a_class))
