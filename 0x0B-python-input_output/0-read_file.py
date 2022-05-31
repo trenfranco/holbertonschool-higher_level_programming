47739a22567cc0f09381b9e31c2fe8bb47b638de
trenfranco <trenfranco@gmail.com>
2022-05-31 13:00:45 -0700

    ex 0 fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """read a file and print to stdout"""
     with open(filename, 'r', encoding="utf-8") as a:
-        print(a.read())
+        print(a.read(), end="")
