df2728ed67716f5ed0df7996dfdbb462297d7b31
trenfranco <trenfranco@gmail.com>
2022-05-30 18:28:30 -0700

    ex 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """New class"""
 
+
 Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
     """New class inhereted form rect"""
     def __init__(self, size):
         super().integer_validator("size", size)
         super().__init__(size, size)
-
