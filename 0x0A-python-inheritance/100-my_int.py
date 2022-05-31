570c6b65513fae540b3159185004ee039fc23a9d
trenfranco <trenfranco@gmail.com>
2022-05-30 19:02:15 -0700

    ex 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,11 +2,11 @@
 """int"""
 
 
-
 class MyInt(int):
     """int"""
 
     def __eq__(self, other):
         return (super().__ne__(other))
+
     def __ne__(self, other):
         return (super().__eq__(other))
