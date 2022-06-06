324d30001a67572391a7c7fbf73988782b9b3751
trenfranco <trenfranco@gmail.com>
2022-06-06 15:21:32 -0700

    ex 0 and README

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""New class"""
+
+
+class Base:
+    """Class Base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
