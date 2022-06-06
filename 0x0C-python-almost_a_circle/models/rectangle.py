62dcf563b89d6d7e6f40ee0841567987eea7fccc
trenfranco <trenfranco@gmail.com>
2022-06-06 16:41:52 -0700

    ex 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,7 +77,11 @@ class Rectangle(Base):
 
     def display(self):
         """display rectangle"""
+        for n in range(self.__y):
+            print()
         for i in range(self.__height):
+            for s in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
