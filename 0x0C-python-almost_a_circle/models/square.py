7a93543af420fdd8eb75c174f92d61be6135649f
trenfranco <trenfranco@gmail.com>
2022-06-06 18:18:38 -0700

    ex 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,12 @@ class Square(Rectangle):
         """overriding str"""
         return (f"[Square] ({self.id}) {self.x}/{self.y}" +
                 f" - {self.width}")
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
