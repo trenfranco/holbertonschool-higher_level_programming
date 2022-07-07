5d4aae7f55d9313ec249bc8e6186ffada112d0cf
trenfranco <trenfranco@gmail.com>
2022-07-06 19:39:25 -0700

    ex 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,3 @@
+-- creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE, state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id), name VARCHAR(256) NOT NULL)
