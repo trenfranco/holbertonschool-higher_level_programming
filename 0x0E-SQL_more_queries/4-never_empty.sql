bd67ee8d7ba643f160f69f6334e450737196bef3
trenfranco <trenfranco@gmail.com>
2022-07-06 19:14:03 -0700

    ex 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- creates the table id_not_null
 CREATE TABLE IF NOT EXISTS id_not_null (
-	`id` INT DEFAULT 1,
-	`NAME` VARCHAR(256) NOT NULL
+	id INT NOT NULL DEFAULT 1,
+	NAME VARCHAR(256) NOT NULL
 );
