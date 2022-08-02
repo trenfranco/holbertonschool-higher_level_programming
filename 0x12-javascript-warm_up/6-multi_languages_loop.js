0fb9591508e1e1170c09f3ea6d3e6b30242137fc
trenfranco <trenranco@gmail.com>
2022-08-01 17:09:20 -0700

    ex 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*  prints 3 lines  */
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const size = arr.length;
+for (let i = 0; i < size; i++) {
+  console.log(arr[i]);
+}
