a55ab0958c42920ea1458bd2fc943eb5e4af2c4a
trenfranco <trenranco@gmail.com>
2022-08-02 00:28:27 -0700

    ex 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Rectangle class  */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w && h && w >= 0 && h >= 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
