60cc5082799f629ebb81d7b27f423d7645bcdb04
trenfranco <trenfranco@gmail.com>
2022-08-07 15:48:44 -0700

    ex 1 fixed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,6 +13,7 @@ if (__name__ == "__main__"):
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
     db.close()
