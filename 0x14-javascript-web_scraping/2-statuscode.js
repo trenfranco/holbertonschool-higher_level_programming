33a62c31261b0b4a53974ecde7d1c294c9399911
trenfranco <trenfranco@gmail.com>
2022-09-12 20:46:56 +0000

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ const axios = require('axios');
 axios.get(process.argv[2]).then(resp => {
   console.log('code: ' + resp.status);
 })
-  .catch(() => {
-    console.log('code: ' + 404);
+  .catch(error => {
+    console.log('code: ' + error.response.status);
   });
