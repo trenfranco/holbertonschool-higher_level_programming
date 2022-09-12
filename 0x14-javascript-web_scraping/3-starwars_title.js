82e89e1638281aa787f62784a2017d2ef4451824
trenfranco <trenfranco@gmail.com>
2022-09-12 21:52:09 +0000

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//request get to get the data (json)
+const axios = require('axios');
+
+axios
+  .get('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
+  .then(response => {
+    console.log(response.data.title);
+  });
