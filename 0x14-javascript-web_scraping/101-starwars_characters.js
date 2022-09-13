8e9fa33b3fa7cea6b14f2b1c271056b16e095e49
trenfranco <trenfranco@gmail.com>
2022-09-13 02:33:43 +0000

    task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,13 +2,3 @@
 // Script that prints all characters of a Star Wars movie
 
 const axios = require('axios');
-axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
-  .then(function (response) {
-    const characters = response.data.characters;
-    for (let i = 0; i < characters.length; i++) {
-      axios.get(characters[i])
-        .then(function (response2) {
-          console.log(response2.data.name);
-        });
-    }
-  });
